7f0d990b1cd9535fa97702b9108d94fa87e9a374
Marcus Vinicius <marcus.dev95@hotmail.com>
2022-06-09 01:32:46 -0300

    Primeiro...

---
+++ por.js
@@ -0,0 +1,22 @@
+function AbrirMenu(){
+
+    let MenuArea = document.querySelector('nav');
+
+    if(MenuArea.classList.contains('menu-opened')==true){
+
+        MenuArea.classList.remove('menu-opened');
+
+
+    }
+    else{
+
+        MenuArea.classList.add('menu-opened');
+    }
+
+
+}
+
+
+    
+    
+    
