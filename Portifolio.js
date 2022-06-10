578cc9e3a981c3b115a71a1095e6315f49771bb4
Marcus Vinicius <marcus.dev95@hotmail.com>
2022-06-10 02:08:48 -0300

    Alterando Js

--- Portifolio.js
+++ Portifolio.js
@@ -2,8 +2,6 @@ let MenuArea2 = document.querySelector("nav")
 let clickm2 = document.querySelector('.menu-opened')
 clickm2.addEventListener('touchstart', function(){
     
-    
-
     if(MenuArea2.style.width == '35%'){
 
         MenuArea2.style.width ='0';
