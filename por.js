8b5205056233dc0f136eee40085ed580f50c5a69
Marcus Vinicius <marcus.dev95@hotmail.com>
2022-06-10 00:57:35 -0300

    atualizando doc js

--- por.js
+++ por.js
@@ -1,39 +1,3 @@
-/*
-function AbrirMenu(){
-    let MenuArea = document.querySelector("nav");
-
-    if(MenuArea.style.width == '80%'){
-
-        MenuArea.style.width ='0';
-
-    }else{
-
-        MenuArea.style.width ='80%';
-    }
-}
-
-
-
-let MenuArea = document.querySelector("nav")
-let clickm = document.querySelector('.menu-opened')
-
-clickm.addEventListener('click', function(){
-
-    
-
-    if(MenuArea.style.width == '80%'){
-
-        MenuArea.style.width ='0';
-
-    }else{
-
-        MenuArea.style.width ='80%';
-    }
-})
-
-*/
-
-
 let MenuArea2 = document.querySelector("nav")
 let clickm2 = document.querySelector('.menu-opened')
 clickm2.addEventListener('touchstart', function(){
