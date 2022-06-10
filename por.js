2179c19fd7f4709f26366ec5a513f59bbda840a5
Marcus Vinicius <marcus.dev95@hotmail.com>
2022-06-10 00:48:58 -0300

    Atulizando menu mobile

--- por.js
+++ por.js
@@ -11,7 +11,7 @@ function AbrirMenu(){
         MenuArea.style.width ='80%';
     }
 }
-*/
+
 
 
 let MenuArea = document.querySelector("nav")
@@ -28,6 +28,25 @@ clickm.addEventListener('click', function(){
     }else{
 
         MenuArea.style.width ='80%';
+    }
+})
+
+*/
+
+
+let MenuArea2 = document.querySelector("nav")
+let clickm2 = document.querySelector('.menu-opened')
+clickm2.addEventListener('touchstart', function(){
+    
+    
+
+    if(MenuArea2.style.width == '35%'){
+
+        MenuArea2.style.width ='0';
+
+    }else{
+
+        MenuArea2.style.width ='35%';
     }
 })
 
